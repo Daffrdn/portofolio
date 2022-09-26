3c8bd9a9bb2ad8f07d87a1cb2dfd7337da9c297a
unknown <daffa.fahrezy24@gmail.com>
2022-09-26 10:33:31 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,125 @@
+<template>
+  <v-app ligth>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      elevation="0"
+      color="white"
+    >
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn 
+       plain
+       elevation="0"
+       @click="home"
+      >
+        Home
+      </v-btn>
+      <v-btn
+       plain
+       elevation="0"
+       @click="pro"
+      >
+        Projects
+      </v-btn>
+      <v-btn
+       plain
+       elevation="0"
+       @click="exp"
+      >
+        Experience
+      </v-btn>
+      <v-btn
+       plain
+       elevation="0"
+       @click="edu"
+      >
+        Education
+      </v-btn>
+      <v-btn
+       plain
+       elevation="0" 
+       @click="con" 
+      >
+        Contact
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+      dark
+    >
+      <v-btn 
+       plain
+       elevation="0"
+       @click="home"
+      >
+        Home
+      </v-btn>
+      <v-btn
+       plain
+       elevation="0"
+       @click="pro"
+      >
+        Projects
+      </v-btn>
+      <v-btn
+       plain
+       elevation="0"
+       @click="exp"
+      >
+        Experience
+      </v-btn>
+      <v-btn
+       plain
+       elevation="0"
+       @click="edu"
+      >
+        Education
+      </v-btn>
+      <v-btn
+       plain
+       elevation="0" 
+       @click="con" 
+      >
+        Contact
+      </v-btn>
+      <v-spacer/>
+      <span>&copy; {{ new Date().getFullYear() }} Daffa Erdian Fahrezy. Created with Nuxt</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      title: 'LOGO'
+    }
+  },
+  methods: {
+    home(){
+      this.$router.push('/')
+    },
+    pro(){
+      this.$router.push('projects')
+    },
+    edu(){
+      this.$router.push('education')
+    },
+    exp(){
+      this.$router.push('experience')
+    },
+    con(){
+      this.$router.push('contact')
+    },
+  },
+}
+</script>
