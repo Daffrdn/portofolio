b27e1e1d5a1d625ba5bb9c84b1c7d896dbee18b6
unknown <daffa.fahrezy24@gmail.com>
2022-09-27 13:53:57 +0700

    Checkpoint: Add Gmaps

--- pages/contact.vue
+++ pages/contact.vue
@@ -12,6 +12,7 @@
         >
             <h2>Daffa Erdian Fahrezy, 21 Years Old</h2>
             <br>
+            <span>Hi, My name is Daffa Erdian Fahrezy, im from Indonesia, if your need something just call me or text me in the down below... </span>
             <div class="mt-2">
                 <v-btn 
                 icon
@@ -43,13 +44,7 @@
                 <span class="ml-1 black--text">Daffrdn</span>
             </div>
             <br>
-        <v-card
-        elevation="0"
-        height="180"
-        width="450"
-        color="grey"
-        >
-        </v-card>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.091774614524!2d107.60653793998947!3d-6.879608010212437!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e6f1a2c477bf%3A0x69776f70b1f628e6!2s4JC4%2BC4G%2C%20Hegarmanah%2C%20Cidadap%2C%20Bandung%20City%2C%20West%20Java%2040141!5e0!3m2!1sen!2sid!4v1664260156165!5m2!1sen!2sid" width="450" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </v-col>
         <v-col
         class="mx-auto"
@@ -60,16 +55,12 @@
                 <v-col>
                     <span>Name:</span>
                     <v-text-field
-                    label="Outlined"
-                    placeholder="Placeholder"
                     outlined
                     ></v-text-field>
                 </v-col>
                 <v-col>
                     <span>E-mail:</span>
                     <v-text-field
-                    label="Outlined"
-                    placeholder="Placeholder"
                     outlined
                     ></v-text-field>
                 </v-col>
@@ -78,7 +69,6 @@
             <v-textarea
             outlined
             rows="8"
-            label="Outlined textarea"
             ></v-textarea>
             <v-btn 
             outlined 
