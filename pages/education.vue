52e77813915c86babcba6753c54ebed922c9d10b
unknown <daffa.fahrezy24@gmail.com>
2022-09-27 11:11:37 +0700

    wireframe education

--- pages/education.vue
+++ pages/education.vue
@@ -1,7 +1,49 @@
 <template>
-  <div>
+<div>
+  <div><br><br>
     <h1>Education</h1>
-  </div>
+  </div><br>
+  <v-row>
+    <v-col cols="12" md="9">
+      <h2>Undergraduate, STMIK Amik Bandung</h2>
+      <span>
+      I am a 6
+      th-semester student majoring in Informatics Engineering at STMIK
+      ”AMIKBANDUNG”. As a student, I learn many new things in my current
+      environments such as courses, friends, and the atmosphere during lectures. 
+      <br><br>
+      As a student, in academics I got a GPA of 3.60/4.00 during 6 semesters I
+      studied in the dictionary, on campus I have 2 majors and I choose Cyber
+      Security in the 5
+      th
+      -semester, I am also active in the non-academic division, such as participating in the vaccination committee for 3 months, the
+      technology webinar committee and participating in the informatics engineering student association organization.
+      </span>
+    </v-col>
+    <v-col>
+    </v-col>
+  </v-row>
+  <br>
+  <v-divider></v-divider>
+  <br>
+  <v-row>
+    <v-col>
+    </v-col>
+    <v-col cols="12" md="9">
+      <h2>Computer Network Engineering, SMK Putra Pajajaran</h2>
+      <span>
+      In high school, I learned a lot about the basics of computers and internet
+      networks, my first experience was to build a personal computer and create
+      a Local Area Network. 
+      <br><br>
+      And now I am still developing this network science such as making Fiber
+      Optic networks, Performing Test Network and Web Penetration Testing, Maintenance Computer and Networks, Creating Mikrotik Networks and
+      Mobile and 
+      </span>
+    </v-col>
+  </v-row>
+  <br><br>
+</div>
 </template>
 
 <script>
